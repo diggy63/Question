0b3d020cc7ba1336452f52eacb6e16e433245f20
diggyben <daniel.kanczugowski@gmail.com>
2022-08-05 15:32:49 -0700

    first Commit

--- client/components/organisms/Footer/Footer.js
+++ client/components/organisms/Footer/Footer.js
@@ -12,7 +12,7 @@ export default function FooterComponent() {
       <Container>
         <Content className="has-text-centered">
           <p>
-            {`Copyright Ⓒ ${year} MERN Boilerplate. All Rights Reserved.`}
+            {`Please tell me this will change`}
           </p>
         </Content>
       </Container>
--- client/components/organisms/Navigation/Navigation.js
+++ client/components/organisms/Navigation/Navigation.js
@@ -48,7 +48,7 @@ export default function Navigation({ pathname }) {
             component={Link}
           >
             <Title className="logo" size="3">
-              MERN Boilerplate
+              Sebi's Bistro
             </Title>
           </Navbar.Item>
           <div className="navbar-brand-right">
