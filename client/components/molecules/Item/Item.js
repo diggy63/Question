52380f3a7ec7f4e870cdbc13f47fc78b2cce3f84
diggyben <daniel.kanczugowski@gmail.com>
2022-08-09 19:16:31 -0700

    adding some styling to catering page

--- client/components/molecules/Item/Item.js
+++ client/components/molecules/Item/Item.js
@@ -2,6 +2,8 @@ import React, { useState, useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 
 import {attemptToDeleteItem} from '_thunks/items'
+import { faTrashCan } from '@fortawesome/free-solid-svg-icons/faTrashCan';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 export default function item({id,name,description,price}){
     const dispatch = useDispatch()
@@ -10,15 +12,30 @@ export default function item({id,name,description,price}){
         dispatch(attemptToDeleteItem(id))
     }
     return(
-        <div>
-            <div>
-            {name}
-            </div>
-            {description}
-            <div>
-            {price}
-            </div>
-            <button onClick={deleteItem} value={id}>Delete</button>
-        </div>
+        <li className='todo box'>
+            <article className='Media'>
+                <div className='media-content'>
+                    <div className='content'>
+                        <p>
+                            <h1>{name}</h1>
+                        </p>
+                        <p>
+                            Description:  {description}
+                        </p>
+                        <p>
+                            Price: {price}
+                        </p>
+                    </div>
+                    <nav className='level is-mobile'>
+                        <div className='level-right'>
+                            <span className='icon' onClick={deleteItem}>
+                            <FontAwesomeIcon icon={faTrashCan} size="lg" />
+                            </span>
+                        </div>
+                    </nav>
+                </div>
+            </article>
+        </li>
+        
     )
 }
