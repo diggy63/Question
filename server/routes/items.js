e939a61da3a05f0a6d5dd4730313a03e580b7ecb
diggyben <daniel.kanczugowski@gmail.com>
2022-08-15 11:40:36 -0700

    addingcode for debugging

--- server/routes/items.js
+++ server/routes/items.js
@@ -52,6 +52,7 @@ router.post('/', requireAuth, upload.single("photo"), (req, res) => {
   };
 
   s3.upload(params, async function (err, data) {
+    console.log(data)
     const newItem = CatItem({ ...req.body, photoUrl: data.Location });
     newItem.save((err, savedItem) => {
       if (err) {
